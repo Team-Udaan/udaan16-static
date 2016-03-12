b87f679ef5b612a5ea51c5c21abbe15e58d014bb
Dhruvin Gandhi <dhruvin2910@gmail.com>
2016-03-12 21:10:16 +0530

    Add onepage_scroll

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// begin:bootstrap onepage_scroll plugin
+
+$(window).ready(function () {
+  $('.main').onepage_scroll({
+    easing: 'ease-in-out',
+    pagination: false,
+    loop: false
+  });
+});
+
+// end:bootstrap onepage_scroll plugin
