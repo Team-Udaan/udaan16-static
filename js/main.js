3ad04cfb037601311ff9f5bc74f9ecfea69d3dcc
Dhruvin Gandhi <dhruvin2910@gmail.com>
2016-03-13 14:40:57 +0530

    Add homepage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 // begin:bootstrap onepage_scroll plugin
 
-$(window).ready(function () {
+$(document).ready(function () {
   $('.main').onepage_scroll({
     easing: 'ease-in-out',
     pagination: false,
     loop: false
+  });
+});
+
+// end:bootstrap onepage_scroll plugin
+
+// begin:bootstrap onepage_scroll plugin
+
+$(document).ready(function () {
+  $('#u16-tech-event-carousel').owlCarousel({
+    autoPlay: 2000,
+    items: 4
+  });
+
+  $('#u16-non-tech-event-carousel').owlCarousel({
+    autoPlay: 2000,
+    items: 4
   });
 });
 
