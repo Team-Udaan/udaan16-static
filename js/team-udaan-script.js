57543625c06b5e73af554f2432692be482fffb63
maunilswadas <swadasmaunil@gmail.com>
2016-03-21 21:58:49 +0530

    Add team-udaan.html, css/team-udaan-style.css, js/team-udaan-script.js, apex/*, img/badges/team-icon1.svg , img/apex-cover.jpg Update index.html, nights.html

---
+++ js/team-udaan-script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by maunil on 20-03-2016.
+ */
+
+// begin:bootstrap owl-carousel plugin
+
+$(document).ready(function () {
+    $('#u16-gs-lr-carousel').owlCarousel({
+        autoPlay: 4000,
+        items:2
+    });
+
+    $('#u16-apex-carousel').owlCarousel({
+        autoPlay: 4000,
+        items: 6
+    });
+});
+
+// end:bootstrap owl-carousel plugin
